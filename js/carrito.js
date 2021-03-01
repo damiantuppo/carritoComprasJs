a7c068375b51f6f0b1601cc480d357e0fc16ec52
Damian Tuppo <berlininstrumentos@gmail.com>
2021-03-01 16:42:50 -0300

    empezando funciones carrito

--- js/carrito.js
+++ js/carrito.js
@@ -5,8 +5,10 @@ var cabeceraTabla = `<table class="table table-dark">
                             <tr>  
                                 <th>#</th>
                                 <th>Producto</th> 
-                                <th>precio</th> 
-                                <th>opciones</th>
+                                <th>Precio</th>
+                                <th>Cantidad</th>
+                                <th>Total</th>
+                                <th>Opciones</th>
                             </tr>
                         </thead>
                     <tbody>`;
@@ -22,23 +24,28 @@ var footTabla = `</tbody></table>`;
 
     for (let i=0; i<miCarro.length;i++){
         aux=i+1;
-        cuerpoTabla = cuerpoTabla + `<tr><td>`+ aux +`</td><td>`+ productos[miCarro[i].codigo - 1].titulo+ `</td><td>$`+productos[miCarro[i].codigo - 1].precio+`</td>
+        cuerpoTabla = cuerpoTabla + `<tr><td>`+ aux +`</td><td>`+ productos[miCarro[i].codigo - 1].titulo+ `</td><td>$`+productos[miCarro[i].codigo - 1].precio+`</td><td>`+miCarro[i].cantidad+`</td><td>$`+productos[miCarro[i].codigo - 1].precio * miCarro[i].cantidad +`</td>
                                 <td>
-                                    <button type="button" class="btn btn-success">+</button>
-                                    <button type="button" class="btn btn-info">-</button>
-                                    <button type="button" class="btn btn-danger" style="margin-left:10px;">Eliminar</button>
+                                    <button type="button" class="btn btn-success" value="`+ miCarro[i].codigo +`">+</button>
+                                    <button type="button" class="btn btn-info" value="`+ miCarro[i].codigo +`">-</button>
+                                    <button type="button" class="btn btn-danger" value="`+ miCarro[i].codigo +`" style="margin-left:10px;">Eliminar</button>
                                 </td>
                             </tr>`;
 
-        totalCarro = totalCarro + productos[miCarro[i].codigo - 1].precio
+        totalCarro = totalCarro + (productos[miCarro[i].codigo - 1].precio * miCarro[i].cantidad);
     }
 
     $('#generarCarrito').html(cabeceraTabla + cuerpoTabla + '<tr><td>TOTAL</td><td>$'+totalCarro+'</td></tr>' +  footTabla);
     
 
 
+
+
+/**
+ * Funciones para manejar el carrito
+ **/    
+
+function EliminarProducto(id, carrito){
     
-
-
-
+}
  
